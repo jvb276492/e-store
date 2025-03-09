848b0628da7a38a8701a1488afe966542ddf2414
Jovan Banks <jovanbanks@Jovans-MacBook.local>
2025-03-08 22:36:30 -0600

    added dark mode feature

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello Next.js</h1>
+      <h1 className="text-2xl font-bold">Home</h1>
     </>
   );
 }
--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -3,11 +3,12 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
+import ModeToggle from "./mode-toggle";
 
 export default function Header() {
   return (
     <header className="w-full border-b">
-      <div className="wrapper flex justify-between items-center p-2">
+      <div className="wrapper flex justify-between items-center p-4">
         <div className="flex itmes-start">
           <Link href="/" className="flex items-center">
             <Image
@@ -23,12 +24,13 @@ export default function Header() {
           </Link>
         </div>
         <div className="space-x-2">
+          <ModeToggle />
           <Button asChild variant="ghost">
             <Link href="/cart">
               <ShoppingCart /> Cart
             </Link>
           </Button>
-          <Button asChild variant="ghost">
+          <Button asChild>
             <Link href="/sign-in">
               <UserIcon /> Sign In
             </Link>
